0a561458e7b32e898fe34f6e585f1aec37e7af73
anoud75 <alanoudsaud75@gmail.com>
2025-12-19 13:44:14 +0000

    Add 11-square.py - Square with custom str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that defines a square using Rectangle."""
+
+    def __init__(self, size):
+        """Initialize the square with size.
+
+        Args:
+            size: The size of the square (positive integer).
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return string representation of the square.
+
+        Returns:
+            String in format [Square] size/size.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
