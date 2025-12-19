cfd8f009ea2d82b7adcfdce4f17846225b74f8f3
anoud75 <alanoudsaud75@gmail.com>
2025-12-19 13:40:21 +0000

    Add 9-rectangle.py - Full Rectangle with area and str

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that defines a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle with width and height.
+
+        Args:
+            width: The width of the rectangle (positive integer).
+            height: The height of the rectangle (positive integer).
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate and return the area of the rectangle.
+
+        Returns:
+            The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation of the rectangle.
+
+        Returns:
+            String in format [Rectangle] width/height.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
